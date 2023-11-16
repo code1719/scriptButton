e459bb395b4ce9e7db266e7ecdcc555db9b2b21b
code1719 <codyhearell@gmail.com>
2023-11-16 14:02:45 -0700

    finished js on pt2

---
+++ readme.md
@@ -0,0 +1,2 @@
+created a new repository on github
+adding to github pages after completion
---
+++ script.js
@@ -0,0 +1,19 @@
+let clicks = 0;
+let countDisplay = document.getElementById("clicks");
+function btnPressed() {
+  console.log(clicks++);
+  countDisplay.innerHTML = clicks;
+}
+
+function changeColor() {
+  let red = document.getElementById("rangeRed").value;
+  let green = document.getElementById("rangeGreen").value;
+  let blue = document.getElementById("rangeBlue").value;
+  let color = "rgb(" + red + "," + green + "," + blue + ")";
+  document.body.style.backgroundColor = color;
+  document.getElementById("colorOutput").innerHTML = ": " + color;
+}
+
+document.getElementById("rangeRed").addEventListener("input", changeColor);
+document.getElementById("rangeGreen").addEventListener("input", changeColor);
+document.getElementById("rangeBlue").addEventListener("input", changeColor);
